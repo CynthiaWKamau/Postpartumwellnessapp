7a3691956719adf05e5a3d90813c0827a7321753
CynthiaWKamau <cynthia.kamau@strathmore.edu>
2025-06-19 11:08:38 +0300

    Signup page

---
+++ Frontend/signup.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Signup-Wellness Application</title>
+
+    </head>
+    <body>
+        <div class="signup-box">
+            <h2>Choose your role</h2>
+<form action="" method="post" autocomplete="off">
+    <select name="role" required>
+        <option value="">--Select Role--</option>
+        <option value="mother">Postpartum Mother</option>
+        <option value="therapist">Therapist</option>
+        <option value="admin">Admin</option>
+    </select>
+    <br>
+    <button type="submit" name="submit">Next</button>
+</form>
+        </div>   
+     </body>
+</html>
