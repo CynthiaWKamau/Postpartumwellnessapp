38e473982054bd5447a571ea31f17498e98afec6
Terry <167083271+167146@users.noreply.github.com>
2025-07-01 00:27:14 +0300

    Mood tracker
    css

--- Wellnessapp/mood_tracker.php
+++ Wellnessapp/mood_tracker.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Mood Tracker | Luna Care</title>
-    <link rel="stylesheet" href="mood.css" />
+    <link rel="stylesheet" href="mood_tracker.css" />
 </head>
 
 <body>
